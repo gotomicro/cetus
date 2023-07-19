dcf433c3a43f77dc3eb8a45710e09416b37f5cf1
kl7sn <mex7.0828@gmail.com>
2023-07-19 12:15:13 +0800

    UI 32 64
    Signed-off-by: kl7sn <mex7.0828@gmail.com>

--- l/zap.go
+++ l/zap.go
@@ -5,12 +5,14 @@ import (
 )
 
 var (
-	I   = zap.Int
-	I64 = zap.Int64
-	I32 = zap.Int32
-	F64 = zap.Float64
-	S   = zap.String
-	B   = zap.Bool
-	E   = zap.Error
-	A   = zap.Any
+	I    = zap.Int
+	I32  = zap.Int32
+	UI32 = zap.Uint32
+	I64  = zap.Int64
+	UI64 = zap.Uint64
+	F64  = zap.Float64
+	S    = zap.String
+	B    = zap.Bool
+	E    = zap.Error
+	A    = zap.Any
 )
