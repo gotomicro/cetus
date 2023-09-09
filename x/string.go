a6be462211dadab6bd28e52133d483db7d82fd7c
kl7sn <mex7.0828@gmail.com>
2023-09-09 22:50:08 +0800

    feat: string hash
    Signed-off-by: kl7sn <mex7.0828@gmail.com>

--- x/string.go
+++ x/string.go
@@ -1,6 +1,7 @@
 package x
 
 import (
+	"hash/fnv"
 	"regexp"
 )
 
@@ -8,4 +9,10 @@ var regInputCheck = regexp.MustCompile(`^[a-zA-Z0-9].$`)
 
 func InputCheck(input string) bool {
 	return regInputCheck.MatchString(input)
+}
+
+func Hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
