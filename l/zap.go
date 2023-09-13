aba09cb8158de865a5e2b66c2df58b48645c791f
sevennt <sevennt.leslie@gmail.com>
2023-09-13 10:58:02 +0800

    rename alias in l

--- l/zap.go
+++ l/zap.go
@@ -5,16 +5,21 @@ import (
 )
 
 var (
-	I    = zap.Int
-	I32  = zap.Int32
-	UI32 = zap.Uint32
-	I64  = zap.Int64
-	UI   = zap.Uint
-	UI64 = zap.Uint64
-	F64  = zap.Float64
-	D    = zap.Duration
-	S    = zap.String
-	B    = zap.Bool
-	E    = zap.Error
-	A    = zap.Any
+	I   = zap.Int
+	I8  = zap.Int8
+	I16 = zap.Int16
+	I32 = zap.Int32
+	I64 = zap.Int64
+	U   = zap.Uint
+	U8  = zap.Uint8
+	U16 = zap.Uint16
+	U32 = zap.Uint32
+	U64 = zap.Uint64
+	F64 = zap.Float64
+	S   = zap.String
+	B   = zap.Bool
+	E   = zap.Error
+	A   = zap.Any
+	D   = zap.Duration
+	T   = zap.Time
 )
