30aa0d3c2d1997bc3e78599cea773cce92c5c610
kl7sn <mex7.0828@gmail.com>
2024-06-12 16:25:08 +0800

    feat: xnet func local ip

--- xnet/ip.go
+++ xnet/ip.go
@@ -72,3 +72,20 @@ func GetOutBoundIP() (ip string, err error) {
 	ip = strings.Split(localAddr.String(), ":")[0]
 	return
 }
+
+func Local() (ip string, err error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+	return "", errors.New("could not find any ip")
+}
