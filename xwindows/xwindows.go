b283a48bab9af8660a04fa4ee2f91e5c576c0835
kl7sn <mex7.0828@gmail.com>
2024-06-21 11:23:03 +0800

    feat(xwindows): provides the ability to push aggregated data within a window of time

---
+++ xwindows/xwindows.go
@@ -0,0 +1,108 @@
+package xwindows
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Debounce struct manages the debounce logic for a single document
+type Debounce struct {
+	windowDuration time.Duration
+	timer          *time.Timer
+	latestEvent    string
+	lock           sync.Mutex
+	lastActiveTime time.Time
+	pushFunc       func(string)
+}
+
+// NewDebounce creates a new Debounce
+func NewDebounce(windowDuration time.Duration, pushFunc func(string)) *Debounce {
+	return &Debounce{
+		windowDuration: windowDuration,
+		lastActiveTime: time.Now(),
+		pushFunc:       pushFunc,
+	}
+}
+
+// pushEvent is the actual logic to push the event
+func (d *Debounce) pushEvent() {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	if d.latestEvent != "" {
+		d.pushFunc(d.latestEvent)
+		d.latestEvent = ""
+	}
+}
+
+// addEvent adds a new event and resets the timer
+func (d *Debounce) addEvent(event string) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	d.latestEvent = event
+	d.lastActiveTime = time.Now()
+
+	if d.timer != nil {
+		d.timer.Stop()
+	}
+
+	d.timer = time.AfterFunc(d.windowDuration, d.pushEvent)
+}
+
+// windowsManager manages multiple documents and their debounceMap
+type windowsManager struct {
+	debounceMap        sync.Map
+	windowDuration     time.Duration
+	cleanupInterval    time.Duration
+	inactivityDuration time.Duration
+	pushFunc           func(string)
+}
+
+// NewWindowsManager creates a new windowsManager
+func NewWindowsManager(windowDuration, cleanupInterval, inactivityDuration time.Duration) *windowsManager {
+	dm := &windowsManager{
+		windowDuration:     windowDuration,
+		cleanupInterval:    cleanupInterval,
+		inactivityDuration: inactivityDuration,
+	}
+	go dm.startCleanupRoutine()
+	return dm
+}
+
+// AddWindow adds a new document with the given ID
+func (dm *windowsManager) AddWindow(docID string, pushFunc func(string)) {
+	dm.debounceMap.Store(docID, NewDebounce(dm.windowDuration, pushFunc))
+}
+
+// RemoveWindow removes the document with the given ID
+func (dm *windowsManager) RemoveWindow(docID string) {
+	dm.debounceMap.Delete(docID)
+}
+
+// AddEvent adds an event to the specified document
+func (dm *windowsManager) AddEvent(windowID string, event string) {
+	if debounce, ok := dm.debounceMap.Load(windowID); ok {
+		debounce.(*Debounce).addEvent(event)
+	}
+}
+
+// startCleanupRoutine starts a goroutine that periodically cleans up inactive documents
+func (dm *windowsManager) startCleanupRoutine() {
+	for {
+		time.Sleep(dm.cleanupInterval)
+		now := time.Now()
+		dm.debounceMap.Range(func(key, value interface{}) bool {
+			debounce := value.(*Debounce)
+			debounce.lock.Lock()
+			inactive := now.Sub(debounce.lastActiveTime) > dm.inactivityDuration
+			debounce.lock.Unlock()
+			if inactive {
+				fmt.Printf("Removing inactive document: %s\n", key.(string))
+				dm.RemoveWindow(key.(string))
+			}
+			return true
+		})
+	}
+}
---
+++ xwindows/xwindows_test.go
@@ -0,0 +1,87 @@
+package xwindows
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestNewWindowsManager(t *testing.T) {
+	type args struct {
+		windowDuration     time.Duration
+		cleanupInterval    time.Duration
+		inactivityDuration time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+		want *windowsManager
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test-1",
+			args: args{
+				windowDuration:     30 * time.Second,
+				cleanupInterval:    10 * time.Second,
+				inactivityDuration: 1 * time.Minute,
+			},
+			want: nil,
+		},
+	}
+	pushFunc1 := func(event string) {
+		fmt.Println("Pushing 1 event:", event)
+		fmt.Println(time.Now().Unix())
+	}
+	pushFunc2 := func(event string) {
+		fmt.Println("Pushing 2 event:", event)
+		fmt.Println(time.Now().Unix())
+	}
+	pushFunc3 := func(event string) {
+		fmt.Println("Pushing 3 event:", event)
+		fmt.Println(time.Now().Unix())
+	}
+	pushFunc4 := func(event string) {
+		fmt.Println("Pushing 4 event:", event)
+		fmt.Println(time.Now().Unix())
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			docManager := NewWindowsManager(tt.args.windowDuration, tt.args.cleanupInterval, tt.args.inactivityDuration)
+			// Adding documents
+			docManager.AddWindow("doc1", pushFunc1)
+			docManager.AddWindow("doc2", pushFunc2)
+			docManager.AddWindow("doc3", pushFunc3)
+			docManager.AddWindow("doc4", pushFunc4)
+
+			// Simulating events being added to different documents
+			go func() {
+				docManager.AddEvent("doc1", fmt.Sprintf("Edit 1 for Document 1, %d", time.Now().Unix()))
+				time.Sleep(10 * time.Second)
+				docManager.AddEvent("doc1", fmt.Sprintf("Edit 2 for Document 1, %d", time.Now().Unix()))
+
+			}()
+
+			go func() {
+				docManager.AddEvent("doc2", fmt.Sprintf("Edit 1 for Document 2, %d", time.Now().Unix()))
+				time.Sleep(20 * time.Second)
+				docManager.AddEvent("doc2", fmt.Sprintf("Edit 2 for Document 2, %d", time.Now().Unix()))
+			}()
+
+			go func() {
+				docManager.AddEvent("doc3", fmt.Sprintf("Edit 1 for Document 3, %d", time.Now().Unix()))
+				time.Sleep(30 * time.Second)
+				docManager.AddEvent("doc3", fmt.Sprintf("Edit 2 for Document 3, %d", time.Now().Unix()))
+			}()
+
+			go func() {
+				docManager.AddEvent("doc4", fmt.Sprintf("Edit 1 for Document 4, %d", time.Now().Unix()))
+				time.Sleep(60 * time.Second)
+				docManager.AddEvent("doc4", fmt.Sprintf("Edit 2 for Document 4, %d", time.Now().Unix()))
+			}()
+
+			// Allow some time for all events to be processed
+			time.Sleep(2 * time.Minute)
+
+		})
+	}
+}
